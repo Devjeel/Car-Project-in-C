6767130c65c535ee646fba866ddde458ead4f132
devjeel <jeelhp02@gmail.com>
2019-11-29 21:28:19 -0500

    Driver class completed

--- Driver.cpp
+++ Driver.cpp
@@ -9,20 +9,33 @@ namespace cs
     Driver::Driver(const char* name, Car& C):car_(C)
     {
        //Complete Code
+       strcpy_s(name_, name);
+       car_ = cref(C);
     }
     
     void Driver::drive()
     {
         //Complete Code
+        car_.accelerate(); //Accelarte and brake though car refence obj.
+        car_.brake();
+        showStatus();
     }
     
     void Driver::stop()
     {
         //Complete Code
+        //Keep Breaking until 0 speed
+        while (car_.speed() == 0){
+            car_.brake();
+        }
+        //print status 
+        showStatus();
     }
     
     void Driver::showStatus()
     {
         //Complete Code
+        cout << name_ << " is driving this car." <<endl;
+        cout << car_ <<endl;
     }
 }
