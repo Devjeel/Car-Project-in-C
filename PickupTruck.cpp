8cdc594e72e7b639eaab35296cc94c4bcccbfcd1
devjeel <jeelhp02@gmail.com>
2019-11-29 21:05:28 -0500

    PickupTruck class completed

--- PickupTruck.cpp
+++ PickupTruck.cpp
@@ -1,3 +1,4 @@
+#include <string.h>
 #include <cstring>
 #include "PickupTruck.h"
 
@@ -8,31 +9,46 @@ namespace cs
     PickupTruck::PickupTruck()
     {
        //Complete Code
+       loadAmount_ = 0;
+       char emptyStr[]{""}; // an empty C-style string
+       strcpy_s(emptyStr, loadedMaterial_); //set the load to empty C-style string
     }
     
     bool PickupTruck::isEmpty() const
     {
        //Complete Code
+       if (loadAmount_ == 0){
+           return true;
+       }
     }
     
     void PickupTruck::load(const char* loadedMaterial, int loadAmount)
     {
         //Complete Code
+        loadAmount_ = loadAmount;
+        strcpy_s(loadedMaterial_, loadedMaterial); //set to load_
     }
     
     void PickupTruck::accelerate()
     {
        //Complete Code
+       int currentSpeed = speed(); //get speed
+       currentSpeed += 20; // Add 20
+       Car::speed(currentSpeed); //Set the new speed
     }
     
     void PickupTruck::brake()
     {
         //Complete Code
+        int currentSpeed = speed(); //get speed
+        currentSpeed -= 5; // Subtract 5
+        Car::speed(currentSpeed); //Set the new speed
     }
     
     void PickupTruck::unload()
     {
         //Complete Code
+        loadAmount_ = 0;
     }
     
     std::ostream& PickupTruck::display(std::ostream& os) const
